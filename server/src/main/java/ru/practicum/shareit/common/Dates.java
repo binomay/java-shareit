4f3de821eba71d95ec07dbf69c7142a49656d6de
Binomay <binom_ay@mail.ru>
2023-05-03 11:28:21 +0300

    Sprint 16: localdatetime

--- server/src/main/java/ru/practicum/shareit/common/Dates.java
+++ server/src/main/java/ru/practicum/shareit/common/Dates.java
@@ -1,13 +1,9 @@
 package ru.practicum.shareit.common;
 
 import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 
 public  class Dates {
     public static LocalDateTime getCurrentDateTime() {
-        ZoneId zoneId = ZoneId.of("Europe/Moscow");
-        ZonedDateTime znd = ZonedDateTime.now(zoneId);
-        return znd.toLocalDateTime();
+        return LocalDateTime.now();
     }
 }
